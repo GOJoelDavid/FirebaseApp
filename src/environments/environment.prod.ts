65d0239347909493111d15556f1511b985c85a58
GOJoelDavid <145133849+GOJoelDavid@users.noreply.github.com>
2024-02-23 10:56:23 -0500

    v.1.4

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBnLZA6hCsZ7RV8u8cvcqRZBORrpbmZxXw",
+    authDomain: "ngbytes-fireauth-5fd1c.firebaseapp.com",
+    projectId: "ngbytes-fireauth-5fd1c",
+    storageBucket: "ngbytes-fireauth-5fd1c.appspot.com",
+    messagingSenderId: "185592609207",
+    appId: "1:185592609207:web:9b97d2a788eeb0b3b49032"
+  }
 };
